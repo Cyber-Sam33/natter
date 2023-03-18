dfaa3481688596fd883f11721887e7effdea94c2
knoori-code <knoori3@gmail.com>
2023-03-18 16:35:37 -0400

    Set up socket.io on backend and added cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
 
-var app = express();
+app.use(cors());
+
+const io = new Server(server, {
+  cors:  {
+    origin: 'http://localhost:3000',
+    method: ['GET', "POST"]
+  }
+})
+
+const server = http.createServer(app);
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +30,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} disconnected`)
+  })
+})
 
 module.exports = app;
