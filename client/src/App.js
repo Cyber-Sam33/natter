65cabae18293d91fbbd59310232e723de10e378c
Cyber-Sam33 <issasammi@yahoo.com>
2023-03-20 13:23:10 -0400

    MessageItem Component and name

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,31 @@ import "./App.css";
 import axios from "axios";
 import io from "socket.io-client";
 import Chat from "./components/Chat";
+import { useEffect } from "react";
 
 const socket = io.connect("http://localhost:8080");
 
 function App() {
+
+  useEffect(() => {
+    socket.on('INITIAL_CONNECTION', payload => {
+      console.log('Initial Connection');
+      console.log(payload);
+    });
+
+    return () => {
+      console.log("Unmounting...");
+      socket.off('INITIAL_CONNECTION');
+
+    };
+  }, []);
+
   return (
     <div className="App">
-      <Chat socket={socket}/>
+      <Chat socket={socket} />
     </div>
   );
 }
+
 
 export default App;
---
+++ client/src/components/MessageItem.js
@@ -0,0 +1,22 @@
+<div className="chat-message-left pb-4">
+  <div>
+    <img
+      src="https://bootdey.com/img/Content/avatar/avatar3.png"
+      className="rounded-circle mr-1"
+      alt="Terry Test" // dynamically add name
+      width="40"
+      height="40"
+    />
+    <div className="text-muted small text-nowrap mt-2">
+      2:42 am
+    </div>
+  </div>
+  <div className="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
+    <div className="font-weight-bold mb-1">
+      {/* dynamicall add name below */}
+      Terry Test
+    </div>
+    {/* correct message varibale added below */}
+    Are you working?
+  </div>
+</div>;
