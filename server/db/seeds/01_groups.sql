7928a94748d221b6ac9f12111d9af87030ce6dfd
Cyber-Sam33 <issasammi@yahoo.com>
2023-03-23 15:45:26 -0400

    Add group seeds

--- server/db/seeds/01_groups.sql
+++ server/db/seeds/01_groups.sql
@@ -116,3 +116,120 @@ VALUES
     'Fashion',
     'https://cdn-icons-png.flaticon.com/512/3050/3050239.png'
   );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    14,
+    'Podcasts',
+    'https://cdn-icons-png.flaticon.com/512/658/658148.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    15,
+    'New Technology',
+    'https://cdn-icons-png.flaticon.com/512/9708/9708993.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    16,
+    'Art',
+    'https://cdn-icons-png.flaticon.com/512/3200/3200013.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    17,
+    'Old-Reddit',
+    'https://cdn-icons-png.flaticon.com/512/1322/1322027.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    18,
+    'Lighthouse Labs',
+    'https://cdn-icons-png.flaticon.com/512/0/740.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    19,
+    'Entrepeneurs',
+    'https://cdn-icons-png.flaticon.com/512/9362/9362253.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    20,
+    'Pets',
+    'https://cdn-icons-png.flaticon.com/512/616/616408.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    21,
+    'Travel',
+    'https://cdn-icons-png.flaticon.com/512/826/826070.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    22,
+    'Space',
+    'https://cdn-icons-png.flaticon.com/512/1067/1067357.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    23,
+    'Stand-Up Comedy',
+    'https://cdn-icons-png.flaticon.com/512/9126/9126457.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    24,
+    'Fishing',
+    'https://cdn-icons-png.flaticon.com/512/1997/1997759.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    25,
+    'Education',
+    'https://cdn-icons-png.flaticon.com/512/1940/1940611.png'
+  );
+
+INSERT INTO
+  groups (id, name, logo)
+VALUES
+  (
+    26,
+    'AI',
+    'https://cdn-icons-png.flaticon.com/512/1693/1693894.png'
+  );
