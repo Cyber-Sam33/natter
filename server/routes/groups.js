b6902ecd294680d555280a85b72f118fb61f55f1
knoori-code <knoori3@gmail.com>
2023-03-24 14:11:29 -0400

    Add functionality to render last 5 messages from the database

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,31 +1,28 @@
-const router = require('express').Router();
-const db = require('../configs/db.config');
+const router = require("express").Router();
+const db = require("../configs/db.config");
 // const { getUserById } = require('../db/queries/users');
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.query(`SELECT * FROM groups`).then((results) => {
     res.json(results.rows);
   });
+});
 
+router.get("/message/:group", (req, res) => {
+  const { group } = req.params;
+  db.query(
+    `SELECT messages.*, groups.name, groups.id as group_id FROM messages JOIN groups ON group_id = groups.id WHERE groups.name = $1 LIMIT 5`,
+    [group]
+  ).then((results) => {
+    res.json(results.rows);
+  });
 });
 
 module.exports = router;
 
-
-
-
-
-
-
-
-
-
-
-
-
 // module.exports = (db) => {
-//   // all routes will go here 
-//   
+//   // all routes will go here
+//
 
 //   return router;
 // };
